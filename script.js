eb6c997ba22d8f460f02f8472516b5a82347639b
Caleb Glorvigen <jcglorvi@gmail.com>
2020-12-10 17:54:48 -0500

    Added sudo code. Added code in generatePassword. Setup initial variables.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var specialCharacters = ["!@#$%^&*()"];
+var numeric = [0,1,2,3,4,5,6,7,8,9];
 
 // Write password to the #password input
 function writePassword() {
@@ -10,13 +14,73 @@ function writePassword() {
 
 }
 
-function generatePassword(){
+function generatePassword() {
 
   // All The Logic
-  
+  var passwordLength = askPasswordLength();
+  var password = [];
+  var ifLowerCase = confirm("Do you want lowercase letters in your password?");
+  console.log();
+  var ifUpperCase = confirm("Do you want uppercase letters in your password?");
+  var ifSpecialChar = confirm("Do you want special characters in your password?");
+  var ifNumeric = confirm("Do you want numbers in your password?");
 
-  return "temporary password";
+  //insert values into password function
+
+  return passwordLength;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// GIVEN I need a new, secure password
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+function insertCriteria(){
+   //for loop for password length
+   //if statements
+   // for every concatenation, add on to counter
+}
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+function askPasswordLength(){
+  var passwordLength = prompt("How many characters in your password?");
+  return passwordLength;
+}
+
+
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// arrays
+// var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+// var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+// var specialCharacters = ["!@#$%^&*()"];
+// var numeric = [0,1,2,3,4,5,6,7,8,9];
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// - conditionals
+// var password = [];
+// var ifLowerCase = confirm("Do you want lowercase letters in your password?");
+// var ifUpperCase = confirm("Do you want uppercase letters in your password?");
+// var ifSpecialChar = confirm("Do you want special characters in your password?");
+// var ifNumeric = confirm("Do you want numbers in your password?");
+
+// for(i = 0; i < passwordlength; i++){ 
+//     // i = random generator
+//     if(ifLowerCase){
+//       password = 
+//     }
+// }
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// function generatePassword
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
